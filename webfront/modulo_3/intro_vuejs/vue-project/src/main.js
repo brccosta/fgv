913d76e41c539e1430064270b910b6ae530c16c8
Bruno Carlos da Cunha Costa <brunocosta@ibict.br>
2025-01-16 10:16:52 -0300

    .

--- webfront/modulo_3/intro_vuejs/vue-project/src/main.js
+++ webfront/modulo_3/intro_vuejs/vue-project/src/main.js
@@ -1,6 +1,11 @@
+// Importa o arquivo CSS principal com os estilos globais da aplicação
 import './assets/main.css'
 
+// Importa a função createApp da biblioteca Vue.js
 import { createApp } from 'vue'
+// Importa o componente principal App do arquivo App.vue
 import App from './App.vue'
 
+// Cria a aplicação Vue usando o componente App e monta ela no elemento HTML com id 'app'
 createApp(App).mount('#app')
+
