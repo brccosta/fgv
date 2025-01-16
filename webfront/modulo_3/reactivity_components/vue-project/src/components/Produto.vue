7d201ef7cc5f123835169a24349ea4dca7e936c6
Bruno Carlos da Cunha Costa <brunocosta@ibict.br>
2025-01-16 15:55:02 -0300

    .

--- webfront/modulo_3/reactivity_components/vue-project/src/components/Produto.vue
+++ webfront/modulo_3/reactivity_components/vue-project/src/components/Produto.vue
@@ -2,30 +2,35 @@
 import { ref } from 'vue'
 import StarRating from './StarRating.vue'
 
+// Define as props que o componente recebe
+// Props são dados que vêm do componente pai
 const props = defineProps({
-  produto: {
-    type: Object,
-    required: true
-  }
+    produto: {
+        type: Object,
+        required: true
+    }
 })
 
+// Cria uma variável reativa para armazenar a avaliação
 const avaliacao = ref(0)
-  
+
+// Esta função é um event handler que será chamada quando o evento 'avaliar' 
+// for emitido pelo componente filho (StarRating)
 function salvarAvaliacao(valor) {
-  avaliacao.value = valor
+    avaliacao.value = valor
 }
 </script>
 
 <template>
     <div class="produto">
-      <h2>{{ produto.nome }}</h2>
-      <p>R$ {{ produto.preco }}</p>
-      
-      <StarRating 
-        :initial-rating="avaliacao" 
-        @avaliar="salvarAvaliacao"
-      />
-      
-      <p>Sua avaliação: {{ "⭐".repeat(avaliacao) }}</p>
+        <h2>{{ produto.nome }}</h2>
+        <p>R$ {{ produto.preco }}</p>
+
+        <!-- O componente StarRating emite um evento chamado 'avaliar' -->
+        <!-- @avaliar é a sintaxe de escuta de eventos (equivalente a v-on:avaliar) -->
+        <!-- Quando o evento 'avaliar' é emitido, a função salvarAvaliacao é chamada -->
+        <StarRating :initial-rating="avaliacao" @avaliar="salvarAvaliacao" />
+
+        <p>Sua avaliação: {{ "⭐".repeat(avaliacao) }}</p>
     </div>
 </template>
