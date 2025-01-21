360031f01c1a10b013ff6976d79ba009dc8ab590
Bruno Carlos da Cunha Costa <brunocosta@ibict.br>
2025-01-21 14:53:14 -0300

    .

--- webfront/modulo_6/vue-project/src/views/AboutView.vue
+++ webfront/modulo_6/vue-project/src/views/AboutView.vue
@@ -10,13 +10,21 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
+// Importando o Axios
 import axios from 'axios'
 
+// Criando uma referência reativa para armazenar os usuários
 const usuarios = ref([])
 
+// Executando a chamada à API quando o componente é montado
 onMounted(() => {
+  // Fazendo uma requisição GET para buscar os usuários
+  // O Axios retorna uma Promise (objeto que representa a 
+  // eventual conclusão ou falha de uma operação assíncrona)
   axios.get('http://localhost:3000/usuarios')
     .then(response => {
+      // Atribuindo os dados recebidos à referência reativa
+      // response.data contém o corpo da resposta da API
       usuarios.value = response.data
     })
 })
