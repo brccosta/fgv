8a4d42871014ace1d71d7dff00fcbb082ab21fb7
Bruno Carlos da Cunha Costa <brunocosta@ibict.br>
2025-01-23 08:14:01 -0300

    .

--- webfront/modulo_7/vue-project/src/router/index.js
+++ webfront/modulo_7/vue-project/src/router/index.js
@@ -17,6 +17,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/state',
+      name: 'state',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/StateView.vue'),
+    },
   ],
 })
 
---
+++ webfront/modulo_7/vue-project/src/views/StateView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+// state
+const count = ref(0)
+
+// actions
+function increment() {
+  count.value++
+}
+</script>
+
+<!-- view -->
+<template>{{ count }}</template>
