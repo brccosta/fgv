29615f67ed03c17a7b43f739744a4aa0e2cf69a9
Bruno Carlos da Cunha Costa <brunocosta@ibict.br>
2025-01-23 13:29:01 -0300

    .

--- webfront/modulo_8/vue-project/tests/helper.spec.js
+++ webfront/modulo_8/vue-project/tests/helper.spec.js
@@ -1,15 +1,10 @@
-import { increment } from './helpers'
+import { expect, test } from 'vitest'
+import { increment } from '../src/helpers'
 
-describe('increment', () => {
-  test('increments the current number by 1', () => {
+test('increments the current number by 1', () => {
     expect(increment(0, 10)).toBe(1)
-  })
+})
 
-  test('does not increment the current number over the max', () => {
-    expect(increment(10, 10)).toBe(10)
-  })
-
-  test('has a default max of 10', () => {
-    expect(increment(10)).toBe(10)
-  })
+test('does not increment the current number over the max', () => {
+    expect(increment(10, 10)).toBe(11)
 })
